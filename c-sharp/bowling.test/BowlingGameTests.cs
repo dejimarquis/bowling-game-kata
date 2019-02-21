4be2e6b07ddb7ba4abbf467ea592e9f7d202de9b
Josh Rolstad <jorolsta@microsoft.com>
2019-02-21 13:30:14 -0800

    Stubbing out the solution for c#

---
+++ c-sharp/bowling.test/BowlingGameTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace bowling.test
+{
+    public class BowlingGameTests
+    {
+        [Fact]
+        public void TestTheTruth()
+        {
+            Assert.True(false);
+        }
+    }
+}
